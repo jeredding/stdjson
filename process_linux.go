a90ba7bb9dc654b0a8649c9d20fb93a24508d12d
Njal Karevoll <njal@karevoll.no>
2016-08-02 22:36:22 +0200

    rather defensive setting of p.Cmd.SysProcAttr.Pdeathsig

--- process_linux.go
+++ process_linux.go
@@ -3,5 +3,9 @@ package stdjson
 import "syscall"
 
 func (p *Process) platformSpecificConfig() {
-	p.Cmd.SysProcAttr = syscall.SysProcAttr{Pdeathsig: syscall.SIGKILL}
+	if p.Cmd.SysProcAttr == nil {
+		p.Cmd.SysProcAttr = &syscall.SysProcAttr{Pdeathsig: syscall.SIGKILL}
+	} else {
+		p.Cmd.SysProcAttr.Pdeathsig = syscall.SIGKILL
+	}
 }
